e7791b34495c71b7c813667466baee5292a43d63
Arman Ahmad <aru44362@gmail.com>
2026-08-17 13:46:40 +0530

    Simple Chain

---
+++ 3.Chains/simple.py
@@ -0,0 +1,28 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+prompt = PromptTemplate(
+    template='Generate 5 interesting facts about {topic}',
+    input_variables=['topic']
+)
+
+model = ChatGroq(
+    model="llama-3.3-70b-versatile"
+)
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+
+result = chain.invoke({'topic':'AOT'})
+print(result)
+
+
+
+#for Visualizing the Chain:
+print(f"\nVISUALISING THE CHAIN:\n")
+chain.get_graph().print_ascii()
